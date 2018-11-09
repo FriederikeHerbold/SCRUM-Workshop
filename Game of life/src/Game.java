c3f131908a52ed63b4b3f76d1d373c76c47cf382
David Nadoba <dnadoba@gmail.com>
2018-11-09 16:06:12 +0100

    Game.isAliveInNextGen

--- Game of life/src/Game.java
+++ Game of life/src/Game.java
@@ -1,6 +1,20 @@
 public class Game {
-
-    public boolean isAliveInNextGen(boolean[][] Feld){
-        return true;
+    static public boolean isAliveInNextGen(boolean[][] grid) {
+        boolean isCenterAlive = grid[1][1];
+        return !(countAliveNeighbours(grid) <2);
     }
+
+   static public int countAliveNeighbours(boolean[][] grid){
+       int aliveNeighbours = 0;
+       for (int x = 0; x < 3; x++) {
+           for (int y = 0; y < 3; y++) {
+               if (x != 1 || y != 1) {
+                   if (grid[x][y]) {
+                       aliveNeighbours++;
+                   }
+               }
+           }
+       }
+       return aliveNeighbours;
+   }
 }
