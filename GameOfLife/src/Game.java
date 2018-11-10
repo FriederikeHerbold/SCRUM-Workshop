a54cd03f8ab7ff8de638bf64f35b8e501277f8a3
David Nadoba <dnadoba@gmail.com>
2018-11-10 09:56:00 +0100

    GameOfLive getGridAt and nextStep

--- GameOfLife/src/Game.java
+++ GameOfLife/src/Game.java
@@ -1,20 +1,80 @@
 public class Game {
-    static public boolean isAliveInNextGen(boolean[][] grid) {
-        boolean isCenterAlive = grid[1][1];
-        return !(countAliveNeighbours(grid) <2);
+
+    public boolean[][] field;
+    public int width = 20;
+    public int height = 20;
+
+    public void crateField() {
+        field = new boolean[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                field[x][y] = (Math.random() % 2 == 1);
+            }
+        }
     }
 
-   static public int countAliveNeighbours(boolean[][] grid){
-       int aliveNeighbours = 0;
-       for (int x = 0; x < 3; x++) {
-           for (int y = 0; y < 3; y++) {
-               if (x != 1 || y != 1) {
-                   if (grid[x][y]) {
-                       aliveNeighbours++;
-                   }
-               }
-           }
-       }
-       return aliveNeighbours;
-   }
+    public boolean[][] nextStep(boolean[][] field) {
+        boolean[][] nextGenField = new boolean[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                nextGenField[x][y] = isAliveInNextGen(getGridAtIndex(field, x, y));
+            }
+        }
+        return nextGenField;
+    }
+
+    public int wrap(int index, int count) {
+        return Math.abs(index) % count;
+    }
+
+    public boolean[][] getGridAtIndex(boolean[][] field, int x, int y) {
+        boolean[][] grid = {
+                {
+                        field[wrap(x - 1, width)][wrap(y - 1, height)],
+                        field[wrap(x - 1, width)][wrap(y, height)],
+                        field[wrap(x - 1, width)][wrap(y + 1, height)],
+                }, {
+                field[wrap(x, width)][wrap(y - 1, height)],
+                field[wrap(x, width)][wrap(y, height)],
+                field[wrap(x, width)][wrap(y + 1, height)],
+        }, {
+                field[wrap(x + 1, width)][wrap(y - 1, height)],
+                field[wrap(x + 1, width)][wrap(y, height)],
+                field[wrap(x + 1, width)][wrap(y + 1, height)],
+        },
+        };
+        return grid;
+    }
+
+    static public boolean isAliveInNextGen(boolean[][] grid) {
+        boolean isCenterAlive = grid[1][1];
+        int aliveNeighbours = countAliveNeighbours(grid);
+        // alive
+        if (isCenterAlive) {
+            if (aliveNeighbours < 2 || aliveNeighbours > 3) {
+                return false;
+            }
+            return true;
+            // dead
+        } else {
+            if (aliveNeighbours == 3) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    static public int countAliveNeighbours(boolean[][] grid) {
+        int aliveNeighbours = 0;
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                if (x != 1 || y != 1) {
+                    if (grid[x][y]) {
+                        aliveNeighbours++;
+                    }
+                }
+            }
+        }
+        return aliveNeighbours;
+    }
 }
--- GameOfLife/src/GameTests.java
+++ GameOfLife/src/GameTests.java
@@ -9,13 +9,34 @@ public class GameTests {
     @Test
     void testLestThan2Neighbours() {
         boolean[][] gird = {
-            {false, false, false},
-            {false, true, false},
-            {false, false, true},
+                {false, false, false},
+                {false, true, false},
+                {false, false, true},
         };
 
         assertFalse(Game.isAliveInNextGen(gird));
+    }
 
+    @Test
+    void testMoreThan3Neighbours() {
+        boolean[][] gird = {
+                {true, true, true},
+                {false, true, false},
+                {false, false, true},
+        };
+
+        assertFalse(Game.isAliveInNextGen(gird));
+    }
+
+    @Test
+    void testAllNeighboursAlive() {
+        boolean[][] gird = {
+                {true, true, true},
+                {true, true, true},
+                {true, true, true},
+        };
+
+        assertFalse(Game.isAliveInNextGen(gird));
     }
     @Test
     void testCountNeibours(){
@@ -33,4 +54,64 @@ public class GameTests {
         };
         assertEquals(2,Game.countAliveNeighbours(gird2));
     }
+    @Test
+    void test2Neighbours() {
+        boolean[][] gird = {
+                {false, false, false},
+                {true, true, false},
+                {false, false, true},
+        };
+
+        assertTrue(Game.isAliveInNextGen(gird));
+    }
+    @Test
+    void test3neighbours() {
+        boolean[][] gird = {
+                {false, true, false},
+                {true, true, false},
+                {false, false, true},
+        };
+
+        assertTrue(Game.isAliveInNextGen(gird));
+    }
+    @Test
+    void testDesdGetsAlive() {
+        boolean[][] gird = {
+                {false, true, false},
+                {true, false, false},
+                {false, false, true},
+        };
+
+        assertTrue(Game.isAliveInNextGen(gird));
+    }
+
+
+    @Test
+    void testWrap() {
+        Game game = new Game();
+        assertEquals(0, game.wrap(-2, 2));
+        assertEquals(1, game.wrap(-1, 2));
+        assertEquals(0, game.wrap(0, 2));
+        assertEquals(1, game.wrap(1, 2));
+        assertEquals(0, game.wrap(2, 2));
+
+    }
+ /*   @Test
+    void testStep() {
+        boolean[][] gird1 = {
+                {false, false, false,false},
+                {true, false, false,false},
+                {false, false, false,false},
+        };
+
+        boolean[][] gird2 = {
+                {false, false, false,false},
+                {false, false, false,false},
+                {false, false, false,false},
+        };
+
+        assertEquals(grid2,Game.);
+    }
+*/
+
 }
